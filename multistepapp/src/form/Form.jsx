020217c3374e56544ef0f8f89b3e5e62e51aa5ab
agbarama <agbaramafavour4@gmail.com>
2024-01-09 11:44:32 +0100

    Made the Form component and saved the inputs in a state.

--- multistepapp/src/form/Form.jsx
+++ multistepapp/src/form/Form.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import styles from "./styles/form.module.css";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [formInputs, setFormInputs] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormInputs({
+      ...formInputs,
+      name: name,
+      email: email,
+      phoneNumber: number,
+    });
+    setName("");
+    setEmail("");
+    setNumber("");
+    console.log(formInputs);
+  };
+
   return (
     <div>
       <div className={styles.formInput}>
@@ -8,7 +28,7 @@ const Form = () => {
           <h1>Personal Info</h1>
           <p>Please provide your name, email address and phone number.</p>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.err}>
             <label for="name" className={styles.name}>
               Name
@@ -24,6 +44,8 @@ const Form = () => {
             id="name"
             name="name"
             placeholder="e.g. Stephen King"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
 
           <div className={styles.err}>
@@ -35,9 +57,11 @@ const Form = () => {
           <input
             // className={jsFormEmail}
             type="email"
-            id="emial"
+            id="email"
             name="email"
             placeholder="e.g. stephenking@lorem.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <div className={styles.err}>
@@ -52,6 +76,8 @@ const Form = () => {
             id="phone"
             name="phone"
             placeholder="e.g. +1 234 567 890"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
           />
 
           <button className={styles.btn}>Next</button>
