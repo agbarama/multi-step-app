cbf9f3087b2e2d05b19fd5d930629b3113f413da
agbarama <agbaramafavour4@gmail.com>
2024-01-11 12:53:34 +0100

    Created the addOns array.

---
+++ multistepapp/src/AddOns/data/addOnsArray.jsx
@@ -0,0 +1,22 @@
+export let addOns = [
+  {
+    name: "Online service",
+    access: "Access to multiple games",
+    price: 1,
+    duration: "mo",
+  },
+
+  {
+    name: "Larger storage",
+    access: "Extra 1TB of cloud save",
+    price: 2,
+    duration: "mo",
+  },
+
+  {
+    name: "Customizable profile",
+    access: "Custom theme on your profile",
+    price: 2,
+    duration: "mo",
+  },
+];
